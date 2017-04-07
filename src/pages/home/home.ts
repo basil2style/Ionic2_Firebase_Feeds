db941c9e2c3f62c3ee2ad92edb834f656320d252
Basil <basiltalias92@gmail.com>
2017-04-07 18:33:10 -0400

    Added const post data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,24 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+const data = [{
+  title : 'post1',
+  body : 'hi!',
+  id : 0
+},
+{
+  title : 'post2',
+  body : 'hi2',
+  id : 1
+},{
+  title : 'post3',
+  body : 'hi2',
+  id : 2
+}]
+
+
+
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
